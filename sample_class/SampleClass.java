e5a7a249d224804078c0168b0366e77b2bcd0d26
Viorel Trusca <viorel.trusca@gmail.com>
2019-07-12 22:20:10 +0300

    My first Java class.
    Checked the portability compiling under Ubuntu WSL and run in Windows 10
    and vice-versa.
    s

---
+++ sample_class/SampleClass.java
@@ -0,0 +1,26 @@
+//package training;
+
+public class SampleClass {
+    public int len = 100;
+    public int[] table = new int[len];
+
+    public SampleClass() {
+        for(int i = 0; i < len; i++) table[i] = i;
+    }
+
+    public String toString()
+    {
+        String ret = "";
+        int i;
+        for (i = 0; i < len - 1; i++)
+            ret += table[i] + ", ";
+        ret += table[i];
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        SampleClass sc = new SampleClass();
+        System.out.println(sc);
+    }
+    
+}
